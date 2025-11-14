462ba8c62bf408cb7c5ba6a6e0031c9f00698466
Josiane <josiane@smtsystems.com.br>
2025-11-13 22:19:58 -0300

    trabalho-web

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnReserva = document.getElementById('btn-reserva');
+
+  btnReserva.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Redireciona para outra página
+    window.location.href = 'reserva.html';
+  });
+});
+
+const form = document.getElementById('form-reserva');
+if (form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const data = document.getElementById('data').value;
+    const pessoas = document.getElementById('pessoas').value;
+
+    const msg = document.getElementById('mensagem');
+    msg.textContent = `🍝 Reserva confirmada para ${nome} no dia ${data} para ${pessoas} pessoa(s)!`;
+    msg.style.color = "#006400";
+    msg.style.fontWeight = "bold";
+
+    form.reset();
+  });
+}
+
